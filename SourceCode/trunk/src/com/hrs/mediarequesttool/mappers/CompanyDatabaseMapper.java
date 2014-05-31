7d6facb115809343ff3d7f01cdc54137b4477d2d
NamPT <NamPT@b1933abb-2b9b-4f07-94d5-a883f29f744f>
2014-05-31 03:44:33 +0000

    Delete mapper related Company

--- SourceCode/trunk/src/com/hrs/mediarequesttool/mappers/CompanyDatabaseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hrs.mediarequesttool.mappers;
-
-import org.apache.ibatis.annotations.Param;
-
-import com.hrs.mediarequesttool.pojos.CompanyDatabase;
-
-public interface CompanyDatabaseMapper {
-	CompanyDatabase get(@Param("company_id") int companyId);
-}
