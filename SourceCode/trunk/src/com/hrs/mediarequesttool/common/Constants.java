1c18081fd9f95b15796994e822c92f26a7d509bd
NamPT <NamPT@b1933abb-2b9b-4f07-94d5-a883f29f744f>
2014-06-13 07:41:42 +0000

    Commit change status SUCCESS and optimize view

--- SourceCode/trunk/src/com/hrs/mediarequesttool/common/Constants.java
+++ SourceCode/trunk/src/com/hrs/mediarequesttool/common/Constants.java
@@ -49,6 +49,14 @@ public class Constants {
 	public final static int OLD_TABLE_TYPE = 1;
 	public final static int DATA_DIFFERENT_TYPE = 2;
 	
+	public final static String STATUS_NEW = "NEW";
+	public final static String STATUS_CONFIRMING = "CONFIRMING";
+	public final static String STATUS_OK = "OK";
+	public final static String STATUS_NG = "NG";
+	public final static String STATUS_DELETED = "DELETED";
+	public final static String STATUS_PROCESSING = "PROCESSING";
+	public final static String STATUS_FINISHED = "FINISHED";
+	
 	public final static String[] NEXT_CONFIRMING = {"OK", "NG"};
 	public final static String[] NEXT_NG = {"CONFIRMING", "DELETED"};
 	
