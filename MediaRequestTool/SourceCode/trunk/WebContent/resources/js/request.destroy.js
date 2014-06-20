76b1072465bbe58501d35c0372ba231eea8f7c1a
NamPT <NamPT@b1933abb-2b9b-4f07-94d5-a883f29f744f>
2014-06-20 00:18:53 +0000

    Comment code phan lien ket vs API Kintone

--- MediaRequestTool/SourceCode/trunk/WebContent/resources/js/request.destroy.js
+++ MediaRequestTool/SourceCode/trunk/WebContent/resources/js/request.destroy.js
@@ -85,7 +85,8 @@ function confirmDestroy(requestId) {
 				text : 'OK',
 				action : function(targetBox) {
 					if (isSet(targetBox._response) && isSet(targetBox._response.url)) {
-						location.href = me.dapps.global['url.context'] + targetBox._response.url;
+						//location.href = me.dapps.global['url.context'] + targetBox._response.url;
+						window.open("", "_self").close();
 					} else if (isSet(targetBox._error) && targetBox._error.status == 403) {
 						location.href = me.dapps.global['url.context'] + "/";
 					} else {
