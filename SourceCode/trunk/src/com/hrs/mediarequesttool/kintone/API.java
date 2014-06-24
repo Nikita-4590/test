b45e1371b9224cc83a968c128556c93238a538ab
NamPT <NamPT@b1933abb-2b9b-4f07-94d5-a883f29f744f>
2014-06-24 03:17:11 +0000

    Format lại file API.java chứ ko thay đổi nội dung

--- SourceCode/trunk/src/com/hrs/mediarequesttool/kintone/API.java
+++ SourceCode/trunk/src/com/hrs/mediarequesttool/kintone/API.java
@@ -21,90 +21,90 @@ import com.hrs.mediarequesttool.kintone.exception.KintoneException;
 import com.hrs.mediarequesttool.pojos.RelationRequest;
 
 public class API {
-  static private final String JSON_MIME = "application/json";
-  static private final String HEADER_HOST = "Host";
-  static private final String HEADER_AUTHORIZATION = "Authorization";
-  static private final String HEADER_CYBOZU_AUTHORIZATION = "X-Cybozu-Authorization";
-  static private final String HEADER_CONTENT_TYPE = "Content-Type";
+	static private final String JSON_MIME = "application/json";
+	static private final String HEADER_HOST = "Host";
+	static private final String HEADER_AUTHORIZATION = "Authorization";
+	static private final String HEADER_CYBOZU_AUTHORIZATION = "X-Cybozu-Authorization";
+	static private final String HEADER_CONTENT_TYPE = "Content-Type";
 
-  private KintoneAPIClientHttpRequestFactory requestFactory;
-  private Gson gson;
-  private Adapter adapter;
+	private KintoneAPIClientHttpRequestFactory requestFactory;
+	private Gson gson;
+	private Adapter adapter;
 
-  private URI kintoneURI;
-  private String kintoneApplicationID;
+	private URI kintoneURI;
+	private String kintoneApplicationID;
 
-  public API() {
-    requestFactory = new KintoneAPIClientHttpRequestFactory();
-    adapter = new Adapter();
-    gson = new Gson();
+	public API() {
+		requestFactory = new KintoneAPIClientHttpRequestFactory();
+		adapter = new Adapter();
+		gson = new Gson();
 
-    // get configuration from .properties
-    kintoneURI = URI.create(PropertiesLoader.instance.getKintoneUrl());
-    kintoneApplicationID = PropertiesLoader.instance.getKintoneApplicationID();
-  }
+		// get configuration from .properties
+		kintoneURI = URI.create(PropertiesLoader.instance.getKintoneUrl());
+		kintoneApplicationID = PropertiesLoader.instance.getKintoneApplicationID();
+	}
 
-  public PostResponse post(RelationRequest request, boolean isUkerukun) throws KintoneException, Exception {
-    return post(adapter.parse(request, isUkerukun));
-  }
+	public PostResponse post(RelationRequest request, boolean isUkerukun) throws KintoneException, Exception {
+		return post(adapter.parse(request, isUkerukun));
+	}
 
-  private PostResponse post(Record record) throws KintoneException {
-    PostResponse postResponse;
-    try {
-      PostData post = new PostData();
-      post.setApp(kintoneApplicationID);
-      post.setRecord(record);
+	private PostResponse post(Record record) throws KintoneException {
+		PostResponse postResponse;
+		try {
+			PostData post = new PostData();
+			post.setApp(kintoneApplicationID);
+			post.setRecord(record);
 
-      ClientHttpRequest request = requestFactory.createRequest(kintoneURI, HttpMethod.POST);
+			ClientHttpRequest request = requestFactory.createRequest(kintoneURI, HttpMethod.POST);
 
-      //PrintWriter writer = new PrintWriter(request.getBody());
-      PrintWriter writer = new PrintWriter(new OutputStreamWriter(request.getBody(), "UTF-8"));
+			// PrintWriter writer = new PrintWriter(request.getBody());
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(request.getBody(), "UTF-8"));
 
-      writer.write(gson.toJson(post));
+			writer.write(gson.toJson(post));
 
-      writer.close();
-      request.getBody().close();
+			writer.close();
+			request.getBody().close();
 
-      ClientHttpResponse response = request.execute();
+			ClientHttpResponse response = request.execute();
 
-      postResponse = gson.fromJson(new InputStreamReader(response.getBody()), PostResponse.class);
-    } catch (Exception e) {
-      throw new KintoneException(e);
-    }
+			postResponse = gson.fromJson(new InputStreamReader(response.getBody()), PostResponse.class);
+		} catch (Exception e) {
+			throw new KintoneException(e);
+		}
 
-    return postResponse;
-  }
+		return postResponse;
+	}
 
-  class KintoneAPIClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
-    private String token;
-    private String host;
+	class KintoneAPIClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
+		private String token;
+		private String host;
 
-    KintoneAPIClientHttpRequestFactory() {
-      String username = PropertiesLoader.instance.getKintoneUsername();
-      String password = PropertiesLoader.instance.getKintonePassword();
+		KintoneAPIClientHttpRequestFactory() {
+			String username = PropertiesLoader.instance.getKintoneUsername();
+			String password = PropertiesLoader.instance.getKintonePassword();
 
-      token = generateToken(username, password);
-      host = PropertiesLoader.instance.getKintoneHost();
-    }
+			token = generateToken(username, password);
+			host = PropertiesLoader.instance.getKintoneHost();
+		}
 
-    @Override
-    public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
-      ClientHttpRequest request = super.createRequest(uri, httpMethod);
+		@Override
+		public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
+			ClientHttpRequest request = super.createRequest(uri, httpMethod);
 
-      request.getHeaders().add(HEADER_CYBOZU_AUTHORIZATION, token);
-      request.getHeaders().add(HEADER_AUTHORIZATION, "Basic " + token);
-      request.getHeaders().add(HEADER_HOST, host);
+			request.getHeaders().add(HEADER_CYBOZU_AUTHORIZATION, token);
+			request.getHeaders().add(HEADER_AUTHORIZATION, "Basic " + token);
+			request.getHeaders().add(HEADER_HOST, host);
 
-      if (httpMethod == HttpMethod.POST) {
-        request.getHeaders().add(HEADER_CONTENT_TYPE, JSON_MIME);
-      }
+			if (httpMethod == HttpMethod.POST) {
+				request.getHeaders().add(HEADER_CONTENT_TYPE, JSON_MIME);
+			}
 
-      return request;
-    }
+			return request;
+		}
 
-    private String generateToken(String username, String password) {
-      byte[] bytes = Base64.encode((username + ":" + password).getBytes());
-      return new String(bytes);
-    }
-  }
+		private String generateToken(String username, String password) {
+			byte[] bytes = Base64.encode((username + ":" + password).getBytes());
+			return new String(bytes);
+		}
+	}
 }
