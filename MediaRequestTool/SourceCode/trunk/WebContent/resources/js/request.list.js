a645e8a46f46495fabbbda74c549e37e7fc8c4c6
ducbv <ducbv@b1933abb-2b9b-4f07-94d5-a883f29f744f>
2014-07-07 09:09:59 +0000

    ducbv change type close current page to go to list page

--- MediaRequestTool/SourceCode/trunk/WebContent/resources/js/request.list.js
+++ MediaRequestTool/SourceCode/trunk/WebContent/resources/js/request.list.js
@@ -4,39 +4,39 @@ var status = undefined;
 $(me.dapps).bind(
 'load',
 function() {
-	
+	function ajaxPostFormSubmit(url, value) {
+		var form = $('<form method="POST" action=' + '"' + url + '"' + '/>');
+		var input = $('<input id="http_request_id" name="http_request_id" value="' + value + '"' + '>');
+		form.append(input);
+		form.submit();
+	};
 	table = $('#request-ajax-table').table({
 		width : '100%',
 		select_row : false,
 		sortable : true,
 		paging : true,
+		method : 'Post',
 		use_hashes : false,
 		sort_data : me.dapps.global['data_binding'],
 		default_sort : 'status',
+		url : me.dapps.global['url.request_list'],
 		no_data_message : me.dapps.ui.enhanced.locale
 				.text('INF100'),
-		url_patterns : {
-			url : me.dapps.global['url.request_list'],
-			page : 'page={page}',
-			sort : 'sort={sort}',
-			sort_direction : 'sort_direction={sort_direction}',
-			query : '{query_name}={query_value}'
-		},
-		data_binds : [ {
-			name : 'id',
-			type : 'text',
-			comparison : 'ilike',
-		},{
-			name : 'status',
-			type : 'text',
-			comparison : 'ilike',
-		}],
-		row_selected : function(row) {
+		row_selected : function(row) {			
 			var requestId = $(row).attr('row-id');
 			var url = me.dapps.global['url.request_redirect']
 					.replace('{request_id}', requestId);
-			window.open(url);
+			ajaxPostFormSubmit(url, $('#stored_httprequestid_input').val());
 		},
+		store_data : [{
+			'id' : 'input_text_search',
+			'query_name' : 'searchText'
+		},{
+			'id' : 'status_select_option',
+			'query_name' : 'status'
+		}],
+		http_request_id : 'stored_httprequestid_input',
+		isResetSortAfterSearch : true,
 		post_load : function(targetTable, e) {
 			if (isSet(e) && e.status != 200) {
 				if (isUnset(me.dapps.global['relation_request.list_error_box'])) {
@@ -85,36 +85,24 @@ function() {
 			}
 		}
 	});
-	
+	table.setDefaultText([{
+		'id' : 'input_text_search',
+		'query_name' : 'searchText'
+	},{
+		'id' : 'status_select_option',
+		'query_name' : 'status'
+	}]);
 	$('#search-relation-request-form').submit(function(e) {
 		e.preventDefault();
 		status = $("#status_select_option option:selected").val();
 		searchParam = $('#input_text_search').val();
 		table.config.default_sort = 'created_at';
 		table.search([ {
-			query_type : 'all',
 			query_name : 'searchText',
 			query_value : searchParam
 		} , {
-			query_type : 'all',
 			query_name : 'status',
 			query_value : status
 		}]);
 	});
 });
-document.onkeydown = function(event) {	
-	if(event.keyCode == 116) {
-		if(isSet(status) && isSet(searchParam)) {
-			event.preventDefault();
-			table.search([ {
-				query_type : 'all',
-				query_name : 'searchText',
-				query_value : searchParam
-			} , {
-				query_type : 'all',
-				query_name : 'status',
-				query_value : status
-			}]);
-		}
-	}
-};
